9fdfddee5558e18e34fecd0bc85fe8f8024b9baf
Manon Gerray <manon.gerray@gmail.com>
2021-02-06 18:29:10 +0100

    :art: Main page structure beginning

---
+++ components/Homepage.js
@@ -0,0 +1,33 @@
+export default function Homepage() {
+  return (
+    <div>
+      <header>
+        <h1>La grande liste des argonautes !</h1>
+        <div>
+          <form>
+            <label>Ajouter un argonaute</label>
+            <input type="text" placeholder="Roger..." />
+            <button type="submit">OK</button>
+          </form>
+        </div>
+      </header>
+      <main>
+        <div>
+          <p>Patrick</p>
+          <p>Patrick</p>
+          <p>Patrick</p>
+          <p>Patrick</p>
+          <p>Patrick</p>
+          <p>Patrick</p>
+          <p>Patrick</p>
+          <p>Patrick</p>
+          <p>Patrick</p>
+          <p>Patrick</p>
+          <p>Patrick</p>
+          <p>Patrick</p>
+          <p>Patrick</p>
+        </div>
+      </main>
+    </div>
+  );
+}
